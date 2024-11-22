688a431faec6ce3107e7f690cdfea7e18a105110
IT-HARISH-R <harishmass27.8@gmail.com>
2024-11-22 13:17:17 +0530

    audio add

--- script.js
+++ script.js
@@ -1,54 +1,84 @@
+loading();
+
+function loading(){
+
+    const load = document.getElementById('loading');
+    load.classList.remove('hidden');
+    
+    setTimeout(() => {
+        load.classList.add('hidden');
+        displayCard();
+    }, 3000);
+}
 const inputArr = [
     {
         name: "hippo",
-        icone: '<i class="fa-solid fa-hippo"></i>'
+        icone: '<i class="fa-solid fa-hippo" style="font-size: 30px;"></i>'
     },
     {
-        name: "cow",
-        icone: '<i class="fa-solid fa-cow"></i>'
+        name: "dove",
+        icone: '<i class="fa-solid fa-dove" style="font-size: 30px;"></i>'
     },
     {
-        name: "otter",
-        icone: '<i class="fa-solid fa-otter"></i>'
+        name: "froge",
+        icone: '<i class="fa-solid fa-frog" style="font-size: 30px;"></i>'
     },
     {
         name: "paw",
-        icone: '<i class="fa-solid fa-paw"></i>'
+        icone: '<i class="fa-solid fa-paw" style="font-size: 30px;"></i>'
     },
     {
         name: "fish",
-        icone: '<i class="fa-solid fa-fish"></i>'
+        icone: '<i class="fa-solid fa-fish-fins" style="font-size: 30px;"></i>'
     },
     {
         name: "spider",
-        icone: '<i class="fa-solid fa-spider"></i>'
+        icone: '<i class="fa-solid fa-spider" style="font-size: 30px;"></i>'
+    },
+    {
+        name: "horse",
+        icone: '<i class="fa-solid fa-horse" style="font-size: 30px;"></i>'
+    },
+    {
+        name: "cat",
+        icone: '<i class="fa-solid fa-cat" style="font-size: 30px;"></i>'
     },
     {
         name: "hippo",
-        icone: '<i class="fa-solid fa-hippo"></i>'
+        icone: '<i class="fa-solid fa-hippo" style="font-size: 30px;"></i>'
     },
     {
-        name: "cow",
-        icone: '<i class="fa-solid fa-cow"></i>'
+        name: "dove",
+        icone: '<i class="fa-solid fa-dove" style="font-size: 30px;"></i>'
     },
     {
-        name: "otter",
-        icone: '<i class="fa-solid fa-otter"></i>'
+        name: "froge",
+        icone: '<i class="fa-solid fa-frog" style="font-size: 30px;"></i>'
     },
     {
         name: "paw",
-        icone: '<i class="fa-solid fa-paw"></i>'
+        icone: '<i class="fa-solid fa-paw" style="font-size: 30px;"></i>'
     },
     {
         name: "fish",
-        icone: '<i class="fa-solid fa-fish"></i>'
+        icone: '<i class="fa-solid fa-fish-fins" style="font-size: 30px;"></i>'
     },
     {
         name: "spider",
-        icone: '<i class="fa-solid fa-spider"></i>'
+        icone: '<i class="fa-solid fa-spider" style="font-size: 30px;"></i>'
+    },
+    {
+        name: "horse",
+        icone: '<i class="fa-solid fa-horse" style="font-size: 30px;"></i>'
+    },
+    {
+        name: "cat",
+        icone: '<i class="fa-solid fa-cat" style="font-size: 30px;"></i>'
     }
+    
 
 ];
+
 let totalclick=0;
 
 let count =0;
@@ -59,7 +89,6 @@ shuffeCards();
 
 const gamebord = document.getElementById('root');
 
-displayCard();
 
 function shuffeCards() {
     for (i = inputArr.length - 1; i >= 0; i--) {
@@ -89,6 +118,8 @@ function flipCard() {
         console.log('nnnnnnnnnnnnnnnnnnn');
         this.innerHTML = inputArr[cardId].icone;
         console.log(flippedCard)
+        flippedCard[0].classList.remove('active');
+        flippedCard[0].classList.add('cursor-not-allowed')
         if (flippedCard.length == 2) {
             setTimeout(chackMatch,400);
         }
@@ -101,12 +132,15 @@ function chackMatch() {
     if (inputArr[card1Id].name === inputArr[card2Id].name) {
         flippedCard[0].classList.remove('active');
         flippedCard[1].classList.remove('active');
+        flippedCard[1].classList.add('cursor-not-allowed')
         count++;
         console.log(`count is : ${count}`)
         console.log(`arr is : ${inputArr.length/2}`)
         chackgame();
     }
     else {
+        flippedCard[0].classList.add('active');
+        flippedCard[0].classList.remove('cursor-not-allowed')
         flippedCard[0].innerHTML = '';
         flippedCard[0].classList.add('bg-red-500',  'cursor-pointer');
         flippedCard[1].innerHTML = '';
@@ -134,10 +168,53 @@ function chackgame(){
             flippedCard = [];
             totalclick=0;
             end.classList.add('hidden')
-            displayCard();
+            loading();
         })
-                
-        // gamebord.innerHTML='You won!';
-        // gamebord.innerHTML.setAttribute('class','text 3xl ')
     }
 }
+
+
+
+
+
+const volum = document.getElementById('volum');
+let ison = true;
+const audio = document.getElementById('myAudio');
+audio.play();
+
+document.addEventListener('load',()=>{
+    audio.play();
+})
+
+volum.addEventListener('click',()=>{
+    const mute = document.getElementById('mute');
+    const unmute = document.getElementById('unmute');
+
+    if(ison){
+        ison=false;
+    }
+    else{
+        ison=true;
+    }
+
+    if(ison){
+        mute.classList.remove('flex');
+        unmute.classList.remove('hidden');
+        unmute.classList.add('flex');
+        mute.classList.add('hidden');
+        console.log(mute);
+        console.log(unmute); 
+        audio.play();
+    }
+    else{
+        mute.classList.remove('hidden');
+        unmute.classList.remove('flex');
+        mute.classList.add('flex')
+        unmute.classList.add('hidden');
+        audio.pause()
+        console.log(mute);
+        console.log(unmute);
+    }
+
+})
+
